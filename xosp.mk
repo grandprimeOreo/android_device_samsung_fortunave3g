e71ad226851cfe5b5f38f6615dfb186ad5e89056
Real man born on semtember <hieunam03@gmail.com>
2018-05-21 18:26:00 +0700

    Update and rename lineage.mk to xosp.mk

--- xosp.mk
+++ xosp.mk
@@ -1,9 +1,10 @@
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/xosp.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/fortunave3g/full_fortunave3g.mk)
 
 # Setup device configuration
 PRODUCT_DEVICE := fortunave3g
-PRODUCT_NAME := lineage_fortunave3g
+PRODUCT_NAME := xosp_fortunave3g
